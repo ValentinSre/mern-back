6fe657da323dbaa4d32a704542fc1fd9a52a54ec
Valentin SEREE <XR6354@equans.com>
2023-04-15 22:59:47 +0200

    upgrade collection loading

--- controllers/collection-controllers.js
+++ controllers/collection-controllers.js
@@ -9,6 +9,8 @@ const { validationResult } = require("express-validator");
 
 const getCollectionByUserId = async (req, res, next) => {
   const userId = req.params.uid;
+  // find the query params : displayMode
+  const displayMode = req.query.displayMode;
 
   let collection;
 
@@ -16,16 +18,10 @@ const getCollectionByUserId = async (req, res, next) => {
     collection = await Collection.find({
       owner: userId,
       possede: true,
-    }).populate({
-      path: "book",
-      populate: [
-        { path: "auteurs", model: "Artist" },
-        { path: "dessinateurs", model: "Artist" },
-      ],
-    });
+    }).populate("book");
   } catch (err) {
     const error = new HttpError(
-      "Fetching collection failed, please try again later",
+      "La recherche de la collection de l'utilisateur a échoué.",
       500
     );
     return next(error);
@@ -37,7 +33,7 @@ const getCollectionByUserId = async (req, res, next) => {
     );
   }
 
-  const collectionToReturn = collection.map((coll) => {
+  const collectionWithBookId = collection.map((coll) => {
     const { book, ...rest } = coll.toObject({ getters: true });
     const { id } = book;
     return { ...book, ...rest, id_book: id };
@@ -45,10 +41,78 @@ const getCollectionByUserId = async (req, res, next) => {
 
   // Fetch editeurs
   let editeurs = [];
-  for (const coll of collectionToReturn) {
+  for (const coll of collectionWithBookId) {
     if (!editeurs.includes(coll.editeur)) {
       editeurs.push(coll.editeur);
     }
+  }
+
+  let collectionToReturn = [];
+  if (displayMode === "bySeries") {
+    for (const coll of collectionWithBookId) {
+      const { serie, version, titre } = coll;
+      const serieToUse = serie ? serie : titre;
+      const versionToUse = version ? " (v" + version + ")" : "";
+      const serieToUseWithVersion = serieToUse + versionToUse;
+      const serieIndex = collectionToReturn.findIndex(
+        (serie) => serie.serie === serieToUseWithVersion
+      );
+      if (serieIndex === -1) {
+        collectionToReturn.push({
+          serie: serieToUseWithVersion,
+          books: [coll],
+        });
+      } else {
+        collectionToReturn[serieIndex].books.push(coll);
+      }
+    }
+
+    for (const serie of collectionToReturn) {
+      serie.books.sort((a, b) => {
+        if (a.tome && b.tome) {
+          return a.tome - b.tome;
+        } else if (a.tome) {
+          return -1;
+        } else if (b.tome) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+    }
+
+    collectionToReturn.sort((a, b) => {
+      return a.serie.localeCompare(b.serie);
+    });
+  } else {
+    // sort by alphabetical order of the book title : the title is the serie + version + tome + titre (if serie, tome and version are not null) or the titre (if serie, tome and version are null
+    collectionToReturn = collectionWithBookId.sort((a, b) => {
+      const {
+        serie: serieA,
+        tome: tomeA,
+        version: versionA,
+        titre: titreA,
+      } = a;
+      const {
+        serie: serieB,
+        tome: tomeB,
+        version: versionB,
+        titre: titreB,
+      } = b;
+      const serieToUseA = serieA ? serieA : titreA;
+      const versionToUseA = versionA ? " (v" + versionA + ")" : "";
+      const tomeToUseA = tomeA ? " - tome " + tomeA : "";
+      const serieToUseWithVersionAndTomeA =
+        serieToUseA + versionToUseA + tomeToUseA;
+      const serieToUseB = serieB ? serieB : titreB;
+      const versionToUseB = versionB ? " (v" + versionB + ")" : "";
+      const tomeToUseB = tomeB ? " - tome " + tomeB : "";
+      const serieToUseWithVersionAndTomeB =
+        serieToUseB + versionToUseB + tomeToUseB;
+      return serieToUseWithVersionAndTomeA.localeCompare(
+        serieToUseWithVersionAndTomeB
+      );
+    });
   }
 
   res.json({
